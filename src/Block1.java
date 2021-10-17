221854f2e9f5ab886676c4cea065335ee17b8da7
Yauheni Pakatashkin <stinkor06@mail.ru>
2021-10-17 21:37:36 +0300

    add task1

--- src/Block1.java
+++ src/Block1.java
@@ -1,6 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Block1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        task1(3,3,3);
+    }
+    public static double task1(double a, double b, double c){
+      //Найти значение функции z = ((a-3)*b/2)+c
+       double z = ((a-3)*b/2)+c;
+        System.out.println(z);
+        return z;
     }
 
 }
